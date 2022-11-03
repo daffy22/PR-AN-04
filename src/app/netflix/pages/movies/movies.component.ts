ebdd63ca4261cdcd0f53e2d5666b2e7bb4f93e3d
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2022-11-02 20:42:16 -0500

    feat: add movies section

--- src/app/netflix/pages/movies/movies.component.ts
+++ src/app/netflix/pages/movies/movies.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
+import { Search } from '../../interfaces/movie';
 
 @Component({
   selector: 'app-movies',
@@ -8,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesComponent implements OnInit {
 
-  constructor() { }
+  popular: Search[] = [];
+  trends: Search[] = [];
+  launches: Search[] = [];
+
+  constructor( private movieService: MovieService ) { }
 
   ngOnInit(): void {
+    this.getMoviesInfo();
   }
 
+  getMoviesInfo() {
+    this.movieService.getMovieByTitleAndType('avatar', 'movie')
+      .subscribe( ({ Search }) => {
+        this.popular = Search;
+      });
+    this.movieService.getMovieByTitleAndType('fast and furious', 'movie')
+      .subscribe( ({ Search }) => {
+        this.trends = Search;
+      });
+    this.movieService.getMovieByTitleAndType('All', 'movie')
+      .subscribe( ({ Search }) => {
+        this.launches = Search;
+      });
+  }
 }
